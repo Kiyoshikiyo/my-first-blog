7a5f515665ba108531f9fe48bebe455d440f69e5
mandursimon <mandursimon@gmail.com>
2020-05-01 14:56:57 +0200

    commit_5

--- building_access/models.py
+++ building_access/models.py
@@ -9,23 +9,23 @@ from django.contrib.auth.models import User
 class Door(models.Model):
     name = models.CharField(max_length=128)
 
-    #def __str__(self):
-    #    return self.name
+    def __str__(self):
+        return self.name
 
 class Group(models.Model):
     name = models.CharField(max_length=128)
-    access_to_doors = models.ManyToManyField(Door)
+    access_to_doors = models.ManyToManyField(Door, blank=True)
 
     def __str__(self):
         return self.name
 
 class Tag(models.Model):
-    uid = models.BinaryField(max_length=10, unique=True, editable=True)
+    uid = models.BinaryField(max_length=15, unique=True, editable=True)
     active = models.BooleanField(default=True)
     note = models.CharField(blank=True, max_length=128)
     owner = models.ForeignKey(User, on_delete=models.PROTECT)
     access_to_doors = models.ManyToManyField(Door)
     groups_member = models.ManyToManyField(Group)
 
-   # def __str__(self):
-    #    return self.uid
+    def __str__(self):
+       return self.uid
