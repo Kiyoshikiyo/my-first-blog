355538dee2d2bde4112e45a45fb152c80c28c43d
mandursimon <mandursimon@gmail.com>
2020-05-09 14:30:58 +0200

    commit_6

--- building_access/models.py
+++ building_access/models.py
@@ -2,6 +2,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+import base64
 
 
 
@@ -13,14 +14,14 @@ class Door(models.Model):
         return self.name
 
 class Group(models.Model):
-    name = models.CharField(max_length=128)
+    name = models.CharField(max_length=128, blank=True)
     access_to_doors = models.ManyToManyField(Door, blank=True)
 
     def __str__(self):
         return self.name
 
 class Tag(models.Model):
-    uid = models.BinaryField(max_length=15, unique=True, editable=True)
+    uid = models.BinaryField(max_length=15, unique=True, editable=False)
     active = models.BooleanField(default=True)
     note = models.CharField(blank=True, max_length=128)
     owner = models.ForeignKey(User, on_delete=models.PROTECT)
@@ -28,4 +29,5 @@ class Tag(models.Model):
     groups_member = models.ManyToManyField(Group)
 
     def __str__(self):
-       return self.uid
+        return base64.b64encode(self.uid).decode('ascii')
+         
--- building_access/urls.py
+++ building_access/urls.py
@@ -3,7 +3,7 @@ from . import views
 from django.contrib import admin
 
 urlpatterns = [
-    path('home', views.home, name='tag-home'),
+    path('home/', views.home, name='tag-home'),
     path('about/', views.about, name='tag-about'),
     path('admin/', admin.site.urls),
 ]
